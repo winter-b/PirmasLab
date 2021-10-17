89624adf5521ec050c1d636191ffb4e65807cb9a
winter-b <85021800+winter-b@users.noreply.github.com>
2021-10-17 11:36:43 -0700

    Revert "virus like feature added"
    This reverts commit 5b663790b0b5f1ce6abca91f4bccaafeee07e2ff.

--- app/src/main/java/com/example/pirmaslab/MainActivity.java
+++ app/src/main/java/com/example/pirmaslab/MainActivity.java
@@ -1,55 +1,39 @@
 package com.example.pirmaslab;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Color;
-import android.media.Image;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mButton;
     private Button mButton2;
     private TextView tv;
-    private ConstraintLayout rl;
-    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rl = findViewById(R.id.rl1);
-        tv = (TextView) findViewById(R.id.textView1);
-        iv = (ImageView) findViewById(R.id.imageView2);
-        iv.setVisibility(View.INVISIBLE);
-
+        tv = (TextView)findViewById(R.id.textView1);
         mButton = findViewById(R.id.button_send);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tv.setText("Hello friend");
-                mButton2.setVisibility(View.VISIBLE);
-            }
-        });
+        mButton.setOnClickListener(this);
         mButton2 = findViewById(R.id.button_send2);
         mButton2.setVisibility(View.INVISIBLE);
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                iv.setVisibility(View.VISIBLE);
-                rl.setBackgroundResource(R.color.black);
-                tv.setTextColor(Color.parseColor("#ff0000"));
-                finishAndRemoveTask();
-            }
-        });
+        mButton2.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.button_send:
+                tv.setText("Hello friend");
+                mButton2.setVisibility(View.VISIBLE);
+            case R.id.button_send2:
+
+        }
+    }
 }
